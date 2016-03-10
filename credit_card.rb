cacb76ee8d443e68b798c4007584099a0f4afeca
Jherez Taylor <JherezTaylor@users.noreply.github.com>
2016-03-10 17:58:36 +0800

    Update credit_card.rb

--- credit_card.rb
+++ credit_card.rb
@@ -11,7 +11,7 @@ class CreditCard
   def initialize(number, expiration_date, owner, credit_network)
     # TODO: initialize the instance variables listed above
     @number = number
-    @expiration = expiration_date
+    @expiration_date = expiration_date
     @owner = owner
     @credit_network = credit_network
   end
