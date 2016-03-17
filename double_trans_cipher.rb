325a2e894020b5f82b1f48edd5a9ac4a9108e9c9
Jherez Taylor <jherez.taylor@gmail.com>
2016-03-17 18:43:36 +0800

    All tests passing, DRY cyptop_spec

--- double_trans_cipher.rb
+++ double_trans_cipher.rb
@@ -1,16 +1,26 @@
+## DoubleTranspositionCipher
 module DoubleTranspositionCipher
   def self.row_swap(origin, dest, random_key)
-    origin.each_with_index{|row, i|
+    origin.each_with_index do |_row, i|
       dest[random_key[i]] = origin[i]
-    }
-    return dest
+    end
+    dest
   end
 
   def self.undo_swap(origin, dest, random_key)
-    origin.each_with_index{|row, i|
-      dest[random_key.index[i]] = origin[i]
-    }
-    return dest
+    origin.each_with_index do |_row, i|
+      dest[random_key.index(i)] = origin[i]
+    end
+    dest
+  end
+
+  def self.init_matrix(string, key)
+    string = string.to_s
+    size = Math.sqrt(string.length).ceil
+    random_key = Array(0...size).shuffle(random: Random.new(key))
+    string += ' ' * ((size**2) - string.length)
+    matrix = string.chars.each_slice(size).to_a
+    [matrix, random_key, size]
   end
 
   def self.encrypt(document, key)
@@ -21,12 +31,7 @@ module DoubleTranspositionCipher
     # 4. sort columns of each row in predictibly random way
     # 5. return joined cyphertext
 
-    document = document.to_s
-    col = (Math.sqrt(document.length)).ceil
-    row = (document.length.to_f / col).ceil
-    random_key = Array(0...col).shuffle(random: Random.new(key))
-    document = document + ' '*((row * col) - document.length)
-    origin_matrix = document.chars.each_slice(col).to_a
+    origin_matrix, random_key, col = init_matrix(document, key)
 
     row_sorted_matrix = Array.new(col) { Array.new(col, 0) }
     col_sorted_matrix = Array.new(col) { Array.new(col, 0) }
@@ -36,24 +41,19 @@ module DoubleTranspositionCipher
 
     col_sorted_matrix = row_swap(row_sorted_matrix, col_sorted_matrix, random_key)
     col_sorted_matrix = col_sorted_matrix.transpose
-    col_sorted_matrix.map {|row| row.join}.join
+    col_sorted_matrix.map(&:join).join
   end
 
   def self.decrypt(ciphertext, key)
-    ciphertext = ciphertext.to_s
-    col = (Math.sqrt(ciphertext.length)).ceil
-    row = (ciphertext.length.to_f / col).ceil
-    random_key = Array(0...col).shuffle(random: Random.new(key))
-    ciphertext = ciphertext + ' '*((row * col) - ciphertext.length)
-    cipher_matrix = ciphertext.chars.each_slice(col).to_a
+    cipher_matrix, random_key, col = init_matrix(ciphertext, key)
 
     col_sorted_matrix = Array.new(col) { Array.new(col, 0) }
     row_sorted_matrix = Array.new(col) { Array.new(col, 0) }
 
-    col_sorted_matrix = undo_swap(cipher_matrix.transpose,col_sorted_matrix,random_key)
+    col_sorted_matrix = undo_swap(cipher_matrix.transpose, col_sorted_matrix, random_key)
     col_sorted_matrix = col_sorted_matrix.transpose
 
     row_sorted_matrix = undo_swap(col_sorted_matrix, row_sorted_matrix, random_key)
-    row_unsorted_matrix.map {|row| row.join}.join.rstrip
+    row_sorted_matrix.map(&:join).join.rstrip
   end
 end
