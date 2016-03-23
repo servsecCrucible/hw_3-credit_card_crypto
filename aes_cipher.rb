91ce32b61ba74e998028cc5b7d86d026be6fc03b
Nicholas Danks <nicholasdanks@hotmail.com>
2016-03-23 11:26:32 +0800

    modified aescipher code

--- aes_cipher.rb
+++ aes_cipher.rb
@@ -1,4 +1,7 @@
 require 'openssl'
+require 'base64'
+require 'json'
+require 'digest/sha1'
 
 module AesCipher
   def self.encrypt(document, key)
@@ -7,9 +10,26 @@ module AesCipher
     #       and ciphertext is the output of AES encryption
     # NOTE: Use base64 for ciphertext so that it is screen printable
     #       Use cipher block chaining mode only!
+    
+    cipher = OpenSSL::Cipher::Cipher.new("aes-256-cbc")
+    cipher.encrypt
+    
+    cipher.key = Digest::SHA1.hexdigest(key.to_s)
+    iv = cipher.random_iv
+    cipher.iv = iv 
+    
+    crypt_blocks = document.map do |item|
+    	cipher.update(item.to_s)
+    end
+    
+    encrypted = crypt_blocks << cipher.final
+    encrypted = encrypted.to_s
+    [iv, encrypted].to_json	
   end
 
   def self.decrypt(aes_crypt, key)
     # TODO: decrypt from JSON output (aes_crypt) of encrypt method above
+
+ 
   end
 end
